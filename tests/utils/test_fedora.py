678ff750cb86caf80d2e80446a35adf51e040405
Jiri Popelka <jpopelka@redhat.com>
2023-12-15 10:54:59 +0100

    Update test

--- tests/utils/test_fedora.py
+++ tests/utils/test_fedora.py
@@ -10,15 +10,15 @@ def test_get_repo_urls():
     TODO: we need more tests, especially for pending releases and rawhide.
     """
     repo_urls = get_repo_urls(
-        "f32", "x86_64", exclude_buildroot=False, exclude_debuginfo=False
+        "f40", "x86_64", exclude_buildroot=False, exclude_debuginfo=False
     )
     assert len(repo_urls) == 3
 
     # check repo names
-    assert "fedora-32-x86_64" in repo_urls
-    assert "fedora-debuginfo-32-x86_64" in repo_urls
-    assert "fedora-buildroot-32-x86_64" in repo_urls
+    assert "fedora-40-x86_64" in repo_urls
+    assert "fedora-debuginfo-40-x86_64" in repo_urls
+    assert "fedora-buildroot-40-x86_64" in repo_urls
 
     # check that the buildroot URL doesn't point to the "latest" repo;
     # the "/latest/" part of the URL should have been replaced by the real repo id
-    assert "/latest/" not in repo_urls["fedora-buildroot-32-x86_64"]
+    assert "/latest/" not in repo_urls["fedora-buildroot-40-x86_64"]
